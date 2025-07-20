f4e2f8157b9d2582d3b35c54ebcddd370892f3e5
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-07-20 16:06:19 -0700

    Crear owner-actualizar.js

---
+++ plugins/owner-actualizar.js
@@ -0,0 +1,29 @@
+import { exec } from 'child_process';
+
+let handler = async (m, { conn }) => {
+  m.reply(`🪷 Actualizando la bot...`);
+
+  exec('git pull', (err, stdout, stderr) => {
+    if (err) {
+      conn.reply(m.chat, `${msm} Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
+      return;
+    }
+
+    if (stderr) {
+      console.warn('Advertencia durante la actualización:', stderr);
+    }
+
+    if (stdout.includes('Already up to date.')) {
+      conn.reply(m.chat, `🪷 La bot ya está actualizada.`, m);
+    } else {
+      conn.reply(m.chat, `🪷 Actualización realizada con éxito.\n\n${stdout}`, m);
+    }
+  });
+};
+
+handler.help = ['update'];
+handler.tags = ['owner'];
+handler.command = ['update'];
+handler.rowner = true;
+
+export default handler;
